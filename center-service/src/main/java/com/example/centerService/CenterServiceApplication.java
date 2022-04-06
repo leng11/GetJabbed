655e6ce74dffa2efe8ddf71879c7734d686102bf
Lawrence Eng <laweng@publicisgroupe.net>
2022-04-06 13:52:27 -0400

    add service component

---
+++ center-service/src/main/java/com/example/centerService/CenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.centerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterServiceApplication.class, args);
+	}
+
+}
---
+++ center-service/src/main/java/com/example/centerService/controller/Controller.java
@@ -0,0 +1,5 @@
+package com.example.centerService.controller;
+
+public class Controller {
+
+}
